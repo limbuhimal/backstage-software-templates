758a2c4f7d89e5f225ee5ec92fbff04c93c3874c
limbuhimal <limbu.himal@outlook.com>
2025-12-04 09:45:04 -0500

    Adds python app software template

---
+++ python-template/template/src/app.py
@@ -0,0 +1,27 @@
+#'/api/v1/details'
+#'/api/v1/healthz'
+
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time' : datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname'  : socket.gethostname(),
+        'message' : 'Hello, This is from Github Actions but running in self hosted runner!!!!!!!!!!!!!!!!!!!!',
+        'env' : '${{values.app_env}}',
+        'app_name' : '${{values.app_name}}' 
+    })
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({'status' : 'up'}), 200
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
